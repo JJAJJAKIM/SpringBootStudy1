6d4e59762bcc2b64c175fb59429b6bf75f705124
JJAJJAKIM <csm9121@gmail.com>
2024-07-12 13:05:45 +0900

    예제 풀이 완료

--- SpringBootStudy1/src/main/java/com/java/controller/JsonController.java
+++ SpringBootStudy1/src/main/java/com/java/controller/JsonController.java
@@ -1,17 +1,29 @@
 package com.java.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.java.dto.FileDTO;
 import com.java.dto.JsonDTO;
 import com.java.mapper.JsonMapper;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestParam;
 
+
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/json")
+@CrossOrigin(origins = "*")
 public class JsonController {
 
 	private final JsonMapper jsonMapper;
@@ -22,7 +34,9 @@ public class JsonController {
 		JsonDTO jsonDto = JsonDTO.builder().build();
 		int no = jsonMapper.test(KEY);
 		if(KEY == no) {
-			jsonDto.setResult(no);
+			Map map = new HashMap<>();
+			map.put("no", no);
+			jsonDto.setResult(map);
 			jsonDto.setMessage("데이터베이스 접속 성공");
 			jsonDto.setStatus(true);
 		} else {
@@ -31,5 +45,42 @@ public class JsonController {
 		}
 		return jsonDto;
 	}
+	
+	@GetMapping("/type/{type:[0-9]+}")
+	public JsonDTO type(@PathVariable("type") int type) {
+		JsonDTO jsonDto = JsonDTO.builder().build();
+		jsonDto.setMessage("파일 목록 실패");
+		jsonDto.setStatus(false);
+		
+		List<FileDTO> list = jsonMapper.findByType(type);
+		if(list.size() > 0) {
+			jsonDto.setResult(list);
+			jsonDto.setMessage("데이터베이스 접속 성공");
+			jsonDto.setStatus(true);
+		}
+		return jsonDto;
+	}
+	
+	@GetMapping("/no/{no:[0-9]+}/type/{type:[0-9]+}")
+	public JsonDTO delete(@PathVariable("no") int no, @PathVariable("type") int type) {
+		JsonDTO jsonDTO = JsonDTO.builder().build();
+		jsonDTO.setMessage("데이터베이스 접속 실패");
+		jsonDTO.setStatus(false);
+		
+		log.info("NO : {}, type : {}", no, type);
+		if(jsonMapper.delete(no) == 1) {
+			List<FileDTO> list = jsonMapper.findByType(type);
+//			if(list.size() >= 0) {
+				jsonDTO.setResult(list);
+				jsonDTO.setMessage("파일 삭제 성공");
+				jsonDTO.setStatus(true);
+//			} else {
+//				
+			}
+			
+//		}
+		return jsonDTO;
+	}
+	
 	
 }
